5d3af98eabf6a62bb7d646cd2e283da6c54842ff
ovhoffer <karilanko@wp.pl>
2023-06-19 12:31:59 +0200

    reorganised methods in table

--- script.js
+++ script.js
@@ -2,9 +2,24 @@ import { Table } from './table.js';
 
 document.addEventListener('DOMContentLoaded', appInit);
 let template = [];
-let columns = [];
+//let columns = [];
 
-
+const options = {
+   columns: [
+      {
+         label: 'country name',
+         trackBy: 'country'
+      },
+      {
+         label: 'name', 
+         trackBy: 'name'
+      },
+      {
+         label: 'domain',
+         trackBy: 'domains'
+      }
+   ]
+}
 async function getData() {
    const response = await fetch('http://universities.hipolabs.com/search?country=United+Kingdom');
    return await response.json();
@@ -12,52 +27,30 @@ async function getData() {
 
 async function setRows() {
    let data = await getData();
-   //console.log(data)
-   if (data.length != 0) { 
-   //onsole.log(data)
-   const keysNumber = Object.keys(data[0]);
-   //console.log(Object.keys(data[0]))
-      let keyArr = [];
-   for (let i = 0; i < keysNumber.length; i++) {
-      keyArr.push(keysNumber[i]);
-      // console.log(keysNums)
-   }
-   template.push(keyArr);
-   for (let i = 0; i < data.length; i++) {
-      template.push(data[i]);
-   }
+  /// console.log(data);
+   
+   // if (data.length != 0) { 
+   // //onsole.log(data)
+   // const keysNumber = Object.keys(data[0]);
+   // //console.log(Object.keys(data[0]))
+   //    let keyArr = [];
+   // for (let i = 0; i < keysNumber.length; i++) {
+   //    keyArr.push(keysNumber[i]);
+   //    // console.log(keysNums)
+   // }
+   // template.push(keyArr);
+   // for (let i = 0; i < data.length; i++) {
+   //    template.push(data[i]);
+   // }
       
-   const newTable = new Table(data, '.main__table');
+   const newTable = new Table(data, '.main__table', options);
    newTable.init();
    newTable.table = document.querySelector('.main__table');
    //console.log(template)
-      return template;
-}
-}
-
-// function setColumns(data, template) {
-//    const keysNumber = Object.keys(data[0])
-//    //console.log(keysNumber.length)
-//    for (let i = 0; i < keysNumber.length; i++) {
-//       let sb = keysNumber[i];
-//       //console.log(sb)
-//       let newObj = {}
-//        newObj['title'] = keysNumber[i];
-//       for (let a = 0; a < data.length; a++) {
-//          //console.log(data[3])
-//          newObj['value' + a] = data[a][sb];
-         
-//          //console.log(newObj)
-//       }
-//       //console.log(newObj)
-//       template.push(newObj)
-//    }
-
-//    console.log(template[1].title)
-
-   
-   //console.log(setRows())
+     // return template;
 // }
+}
+
 
 function appInit() {
 
--- table.js
+++ table.js
@@ -1,7 +1,8 @@
 export class Table {
-  constructor(data, tableClassName) {
+  constructor(data, tableClassName, options) {
       this.data = data,
       this.table = document.querySelector(tableClassName)
+    this.columns = options.columns
    }
    
  
@@ -9,13 +10,14 @@ export class Table {
     this.headersContainer = document.createElement(`div`); 
       this.headersContainer.classList.add('headers__container');
       this.table.appendChild(this.headersContainer);
-    console.log(this.data);
-    for (let i = 0; i < 3; i++) {
+   // console.log(this.columns.length);
+    for (let i = 0; i < this.columns.length; i++) {
       
       this.header = document.createElement(`div`); 
       this.header.classList.add("header");
       this.particle = document.createElement(`p`);
-      this.particle.innerHTML = Object.keys(this.data[i])[i];
+      this.particle.classList.add('header__paragraph')
+      this.particle.innerHTML = this.columns[i].label;
       //console.log(Object.keys(this.data[1]))
       this.header.appendChild(this.particle);
       this.headersContainer.appendChild(this.header);
@@ -24,20 +26,38 @@ export class Table {
       this.row = document.createElement(`div`);
       this.row.classList.add("row");
       this.table.appendChild(this.row);
-      for (let a = 0; a < 3; a++){
+      for (let a = 0; a < this.columns.length; a++){
+        this.actual = Object.values(this.columns[a])[1]
+        //console.log(this.actual)
+        
+        // for (let b = 0; b < Object.values(this.columns[a])[1].length; b++) {
+        //   if (Object.keys(this.data[i])[a] === Object.values(this.columns[a])[1]) {
+        //     //console.log(Object.keys(this.data[i])[a])
+        //   }
+        // }
         this.column = document.createElement(`div`);
         this.column.classList.add("content");
         this.particleTd = document.createElement(`p`);
-        if (typeof (Object.values(this.data[i])[a]) === 'object') {
-          this.particleTd.innerHTML = JSON.stringify(Object.values(this.data[i])[a]);
-          //console.log(JSON.stringify((Object.values(this.data[i])[a])))
+        this.particleTd.classList.add('content__paragraph')
+
+
+        for (let b = 0; b < Object.keys(this.data[1]).length; b++){
+         // console.log(Object.keys(this.data[i])[b])
+          if (Object.keys(this.data[i])[b] === this.actual) {
+            //console.log('pup')
+           //if (typeof (Object.values(this.data[i])[b]) === 'object') {
+          //    this.particleTd.innerHTML = Object.values(this.data[i])[b]);
+              //console.log(JSON.stringify((Object.values(this.data[i])[a])))
+          // }
+          // else {
+              this.particleTd.innerHTML = Object.values(this.data[i])[b];
+          //  }
+            //console.log(Object.values(this.data[i])[i])
+            this.column.appendChild(this.particleTd);
+            this.row.appendChild(this.column);
+          }
         }
-        else {
-          this.particleTd.innerHTML = Object.values(this.data[i])[a];
-        }
-        //console.log(Object.values(this.data[i])[i])
-        this.column.appendChild(this.particleTd);
-        this.row.appendChild(this.column);
+    
       }
     }
 
